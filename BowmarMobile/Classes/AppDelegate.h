8124c2d0bb5456e0f9ac9b301dae1b8cc296e64a
David Stuart <davidstuart@David-Stuarts-17-MacBook-Pro-2.local>
2012-01-07 11:44:43 -0600

    Initial Commit

---
+++ BowmarMobile/Classes/AppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.h
+//  BowmarMobile
+//
+//  Created by David Stuart on 1/7/12.
+//  Copyright U.S. Army Corps of Engineers 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PhoneGapDelegate.h>
+#else
+	#import "PhoneGapDelegate.h"
+#endif
+
+@interface AppDelegate : PhoneGapDelegate {
+
+	NSString* invokeString;
+}
+
+// invoke string is passed to your app on launch, this is only valid if you 
+// edit BowmarMobile.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
+@property (copy)  NSString* invokeString;
+
+@end
+
