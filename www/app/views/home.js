e7bde275d5953874cc1858bd5446d8fe20a6362f
jp36 <jpettitt@Jamess-MacBook-Pro.local>
2012-01-13 21:46:22 -0600

    set up home page

--- www/app/views/home.js
+++ www/app/views/home.js
@@ -1,10 +1,41 @@
-Bowmar.views.Home = Ext.extend(Ext.Panel, {
-                                   title: "Home",
-                                   iconCls: "home",
-                                    initComponent: function() {
-                                        //app.stores.contacts.load();
-                                        Bowmar.views.Home.superclass.initComponent.apply(this, arguments);
-                                    }
-                                    });
+//  var name = {
+//            xtype: 'panel',
+//                       html: '<h1>Bowmar Baptist Church</h1>',
+//                       flex:3
+//                           };
 
-Ext.reg('home', Bowmar.views.Home);
+    Bowmar.views.Home = Ext.extend(Ext.Panel, {
+                               title: "Home",
+                               iconCls: "home",
+                                   styleHtmlContent: true,
+                                   layout:'vbox',
+                                   
+                               initComponent: function() {
+                                    Ext.apply(this, {
+                                              defaults : {
+                                                xtype:'button',
+                                                width:'250',
+                                                text:"Text Missing",
+                                                flex:1,
+                                                style:{marginBottom:'15px'}
+                                              },
+                                              items: [
+                                                //name, missionStatementButton, staffButton, announcementsButton
+                                                      {xtype: 'panel', 
+//                                                        html:'<h2>Bowmar Baptist Church</h2>',
+                                                        flex:3,
+                                                        layout:'vbox',
+                                                      width:'100%',
+                                                      defaults: {xtype:'panel', flex:1},
+                                                      items: [{},{html:'<h2>Bowmar Baptist Church</h2>',},{}]
+                                                      },
+                                                      {text: "Mission Statement"}, {text: "Staff"},
+                                                      {text: "Announcements", style:{marginTop: '50px'}}
+                                              ]
+                                     });
+                                   
+                                   Bowmar.views.Home.superclass.initComponent.apply(this, arguments);
+                                    }
+                               });
+
+    Ext.reg('home', Bowmar.views.Home);
